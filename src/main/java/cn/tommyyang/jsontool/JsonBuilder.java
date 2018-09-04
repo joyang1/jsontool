616c585d1618508c61b28b1080d8f7cac0c7cd13
tommy <tingzai.yang@gmail.com>
2018-09-04 19:15:58 +0800

    init

---
+++ src/main/java/cn/tommyyang/jsontool/JsonBuilder.java
@@ -0,0 +1,48 @@
+package cn.tommyyang.jsontool;
+
+import com.google.gson.JsonElement;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
+
+/**
+ * Created by TommyYang on 2018/9/4.
+ */
+public interface JsonBuilder {
+
+    JsonBuilder message(String message);
+
+    JsonBuilder message(Supplier<String> message);
+
+    JsonBuilder map(String key, Map map);
+
+    JsonBuilder map(String key, Supplier<Map> map);
+
+    JsonBuilder list(String key, List list);
+
+    JsonBuilder list(String key, Supplier<List> list);
+
+    JsonBuilder field(String key, Object value);
+
+    JsonBuilder field(String key, Supplier value);
+
+    JsonBuilder strField(String key, String value);
+
+    JsonBuilder intField(String key, int value);
+
+    JsonBuilder longField(String key, long value);
+
+    JsonBuilder json(String key, JsonElement jsonElement);
+
+    JsonBuilder json(String key, Supplier<JsonElement> jsonElement);
+
+    JsonBuilder exception(String key, Exception exception);
+
+    //JsonLogger stack();
+
+    JsonBuilder getLogger();
+
+    String toString();
+
+}
